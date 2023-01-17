98b9842f5ba5269289aa31ef21415ea57775880e
Luis <luisvillarreal.lgvc@hotmail.com>
2023-01-16 22:59:43 -0600

    Create models

---
+++ app/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model{
+    protected $table = 'post';
+
+    //Many to One
+    public function user(){
+        return $this->belongsTo('App\User','user_id');
+    }
+
+    //Many to One
+    public function category(){
+        return $this->belongsTo('App\Category','category_id');
+    }
+}
