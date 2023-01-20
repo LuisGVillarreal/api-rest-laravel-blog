6494f798f66882b0d01fde4f9d8d1a304578f555
Luis <luisvillarreal.lgvc@hotmail.com>
2023-01-19 20:34:10 -0600

    Action register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,20 +3,66 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller{
-    
-    public function test(Request $request){
-        return "Action 'test' from UserController";
-    }
 
-    public function register(Request $request){
-        $name = $request->input('name');
-        $surname = $request->input('surname'); 
-        return "Action 'register' from User. Name: $name $surname";
-    }
+	public function test(Request $request){
+		return "Action 'test' from UserController";
+	}
 
-    public function login(Request $request){
-        return "Action 'login' from User";
-    }
+	public function register(Request $request){
+		//Get user data by Post
+		$json = $request->input("json", null);
+		$params = json_decode($json);
+		$params_array = array_map('trim', json_decode($json, true)); //Clean
+
+		if (!empty($params) && !empty($params_array)) {
+			//Validate data
+			$validate = \Validator::make($params_array, [
+				'name'		=> 'required|alpha',
+				'surname'	=> 'required|alpha',
+				'email'		=> 'required|email|unique:users',
+				'password'	=> 'required'
+			]);
+
+			if ($validate->fails()) {
+				$data = array(
+					'status'	=> 'error',
+					'code'		=> 400,
+					'message'	=> "User has not been created",
+					'errors'	=> $validate->errors()
+				);
+			}else{
+				//Save user
+				$pwd = password_hash($params->password, PASSWORD_BCRYPT, ['cost' => 4]);
+				$user = new User();
+				$user->name = $params_array['name'];
+				$user->surname = $params_array['surname'];
+				$user->role = "USER";
+				$user->email = $params_array['email'];
+				$user->password = $pwd;
+				$user->save();
+
+				$data = array(
+					'status'	=> 'success',
+					'code'		=> 200,
+					'message'	=> "User created successfully",
+					'user'		=> $user
+				);
+			}
+		}else{
+			$data = array(
+				'status'	=> 'error',
+				'code'		=> 400,
+				'message'	=> "Data sent are not correct"
+			);
+		}
+
+		return response()->json($data, $data['code']);
+	}
+
+	public function login(Request $request){
+		return "Action 'login' from User";
+	}
 }
