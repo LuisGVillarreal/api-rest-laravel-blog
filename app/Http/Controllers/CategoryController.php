f6f0a57b66f63e952cc846a399d8f85a95046bf5
Luis <luisvillarreal.lgvc@hotmail.com>
2023-01-20 22:17:37 -0600

    'index' & 'show' Action

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Category;
 
 class CategoryController extends Controller{
-    public function test(Request $request){
-        return "Action 'test' from CategoryController";
+
+	//Get all categories
+    public function index(){
+    	$categories = Category::all();
+        return response()->json([
+        	'status'		=> "success",
+        	'code'			=> 200,
+        	'categories'	=> $categories
+        ]);
     }
+
+	//Get a category
+	public function show($id) {
+		$category = Category::find($id);
+		if (is_object($category)) {
+			$category = array(
+	        	'status'		=> "success",
+	        	'code'			=> 200,
+	        	'category'		=> $category
+	        );
+		} else {
+			$category = array(
+	        	'status'		=> "Error",
+	        	'code'			=> 404,
+	        	'message'		=> "Category is not found",
+	        );
+		}
+		return response()->json($category, $category['code']);
+	}
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Post;
-use App\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,21 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test-orm', function () {
-    $posts = Post::all();
-    foreach($posts as $post){
-        echo "<h1>{$post->title}</h1>";
-        echo "<span style='color:gray;'>{$post->user->name} - {$post->category->name}</span>";
-        echo "<p>{$post->content}</p>";
-        echo "<hr>";
-    }
-});
 
 //Api Route
-Route::get('/user/test','UserController@test');
-Route::get('/category/test','CategoryController@test');
-Route::get('/post/test','PostController@test');
-
 //User
 Route::post('/api/register','UserController@register');
 Route::post('/api/login','UserController@login');
@@ -41,6 +26,9 @@ Route::put('/api/user/update','UserController@update');
 Route::post('/api/user/upload','UserController@upload')->middleware('api.auth');
 Route::get('/api/user/avatar/{filename}','UserController@getImage');
 Route::get('/api/user/detail/{id}','UserController@detail');
+
+//Category
+Route::resource('/api/category', 'CategoryController');
 
 
 
